f85c4fd8ea9aadbf10e95defe542da0c34d9cd5b
Will Quanstrom <wquanstr215@gmail.com>
2021-01-04 18:28:18 -0600

    Initial check-in of module JokesWebAppMVC

---
+++ JokesWebAppMVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebAppMVC.Models;
+
+namespace JokesWebAppMVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebAppMVC.Models.Joke> Joke { get; set; }
+    }
+}
---
+++ JokesWebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JokesWebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
